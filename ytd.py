8351387de09d37e3ff5ac47b51ae9585494ebb59
monolit <53448546+monolit@users.noreply.github.com>
2021-09-30 01:29:36 +0300

    Update ytd.py

--- ytd.py
+++ ytd.py
@@ -2,7 +2,7 @@
 #	#
 #	#
 
-# requires: pytube wget moviepy Pillow
+# requires: pytube wget moviepy Pillow cryptg sponsorblock
 
 import os
 from .. import loader, utils
@@ -37,9 +37,31 @@ async def ytd(self, message):
 	url=reply.message
 	yt=YouTube(url)
 	thumb=wget.download(yt.thumbnail_url)
-	vvideo=yt.streams.filter(progressive=True).order_by('resolution').desc().first().download()
+	name=yt.streams.filter(progressive=True).order_by('resolution').desc().first().download()
+	vvideo=await cutter(self, message, name)
 	await message.edit("uplovd")
 	await self.client.send_file(message.to_id, vvideo, caption=f"<a href={url}>{yt.title}</a>", reply_to=reply, supports_streaming=True, duration=round(yt.length), thumb=thumb)
 	os.remove(vvideo)
 	os.remove(thumb)
 	await message.delete()
+
+async def cutter(self, message, filer):
+	clip=VideoFileClip(name)
+	secs=clip.duration
+	import sponsorblock as sb;import os
+	cli = sb.Client()
+	segments=cli.get_skip_segments(url)
+	z=0;a=[]
+	from os import system as s
+	segments.reverse
+	## oh shii*
+	for _ in segments:
+		z+=1;kek="228.mp4"
+		s(f'ffmpeg -y -ss 0 -i "{name}" -t {_.start} -c copy "{kek}"')
+		a.append(kek)
+		z+=1;kek="337.mp4"
+		s(f'ffmpeg -y -ss {_.end} -i "{name}" -to {secs} -c copy "{kek}"')
+		a.append(kek)
+		s(f'ffmpeg -y -i "concat:228.mp4|337.mp4" -c copy out.mp4')
+		s(f"rm -rf {name}");s(f"mv out.mp4 {name}")
+	return 'out.mp4'
