52d79a32b824f22d7a9bf383def08a596bceba17
monolit <53448546+monolit@users.noreply.github.com>
2022-11-24 17:18:10 +0300

    Update asciiart.py

--- asciiart.py
+++ asciiart.py
@@ -57,7 +57,7 @@ class asciiartMod(loader.Module):
         new_image.paste(image, (0, 0), image)
         new_image.convert('RGB').save('RESULT.jpg', "JPEG", quality=100)
         await reply.reply(file='RESULT.jpg', force_document=True)
-        os.remove('RESULT.png')
+        os.remove('RESULT.jpg')
         await message.delete()
         await message.edit("Там пиздец, фото нет")
 
